b9d99e4536902df8134dc9bf3645aa0f01d670d9
Aniket Mokql <mokal@localhost.localdomain>
2026-04-14 07:05:06 +0000

    solid pattern done

--- DSA/src/main/java/org/example/Patterns/BasicPattern.java
+++ DSA/src/main/java/org/example/Patterns/BasicPattern.java
@@ -1,7 +1,119 @@
 package org.example.Patterns;
 
+import java.util.regex.Pattern;
+
 class BasicPattern {
     public static void main(String[] args) {
         System.out.println("Basic Pattern.");
+
+        // System.out.println("Solid Square Pattern");
+        // // * * * *
+        // // * * * *
+        // // * * * *
+        // // * * * *
+        // int n = 4;
+
+        // for(int row = 1; row <= n; row++){
+        // for(int col = 1; col <= n; col++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // System.out.println("Solid Rec Pattern.");
+        // // * * * * *
+        // // * * * * *
+        // // * * * * *
+        // int n = 3;
+        // for(int row = 1; row <= n; row++){
+        // for(int col = 1; col <= 5; col++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();s
+        // }
+
+        // // SOLID PATTERN RIGTH ANGLE TRANGLE
+        // // *
+        // // * *
+        // // * * *
+        // // * * * *
+        // // * * * * *
+        // for(int row = 1; row <= 5; row++){
+        // for(int col = 1; col <= row; col++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // // SOLID ROMBUS PATTERN
+        // // * * * * *
+        // // * * * * *
+        // // * * * * *
+        // // * * * * *
+        // int n =5;
+        // for(int row =1; row<=n;row++){
+        // for(int col=1; col<=n-row;col++){
+        // System.out.print(" ");
+        // }
+
+        // for(int col=1;col<=n;col++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // // LEFT PATTERN
+        // // * * * * *
+        // // * * * *
+        // // * * *
+        // // * *
+        // // *
+        // int n = 5;
+        // for (int row = 1; row <= n; row++) {
+        // for (int col = 1; col <= n - row + 1; col++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // // SOLID PYRIMID Pattern
+        // // *
+        // // * * *
+        // // * * * * *
+        // // * * * * * * *
+        // // * * * * * * * * *
+        // int n = 5;
+
+        // for (int row = 1; row <= n; row++) {
+        // for (int col = 1; col <= n - row; col++) {
+        // System.out.print(" ");
+        // }
+        // for (int col = 1; col <= 2 * row - 1; col++) {
+        // System.out.print("* ");
+
+        // }
+        // System.out.println();
+        // }
+
+        // // INVERTED PATTERN
+
+        // int n = 4;
+        // // * * * * * * *
+        // // * * * * *
+        // // * * *
+        // // *
+        // for (int row = 1; row <= n; row++) {
+        // for (int col = 1; col <= row - 1; col++) {
+        // System.out.print(" ");
+        // }
+        // for (int col = 1; col <= 2 * n - 2 * row + 1; col++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+
+        
+
     }
 }
