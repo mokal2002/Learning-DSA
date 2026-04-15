29a2495f25b7522c1ebd0f4e7908ef7133859691
root <root@localhost.localdomain>
2026-04-15 11:36:24 +0000

    Basic Pattern Done

--- DSA/src/main/java/org/example/Patterns/BasicPattern.java
+++ DSA/src/main/java/org/example/Patterns/BasicPattern.java
@@ -1,7 +1,5 @@
 package org.example.Patterns;
 
-import java.util.regex.Pattern;
-
 class BasicPattern {
     public static void main(String[] args) {
         System.out.println("Basic Pattern.");
@@ -46,16 +44,16 @@ class BasicPattern {
         // }
 
         // // SOLID ROMBUS PATTERN
-        // // * * * * *
-        // // * * * * *
-        // // * * * * *
+        // // - - - - * * * * *
+        // // - - - * * * * *
+        // // - - * * * * *
+        // // - * * * * *
         // // * * * * *
         // int n =5;
         // for(int row =1; row<=n;row++){
         // for(int col=1; col<=n-row;col++){
-        // System.out.print(" ");
+        // System.out.print("- ");
         // }
-
         // for(int col=1;col<=n;col++){
         // System.out.print("* ");
         // }
@@ -77,16 +75,16 @@ class BasicPattern {
         // }
 
         // // SOLID PYRIMID Pattern
-        // // *
-        // // * * *
-        // // * * * * *
-        // // * * * * * * *
+        // // - - - - *
+        // // - - - * * *
+        // // - - * * * * *
+        // // - * * * * * * *
         // // * * * * * * * * *
         // int n = 5;
 
         // for (int row = 1; row <= n; row++) {
         // for (int col = 1; col <= n - row; col++) {
-        // System.out.print(" ");
+        // System.out.print("- ");
         // }
         // for (int col = 1; col <= 2 * row - 1; col++) {
         // System.out.print("* ");
@@ -99,12 +97,12 @@ class BasicPattern {
 
         // int n = 4;
         // // * * * * * * *
-        // // * * * * *
-        // // * * *
-        // // *
+        // // - * * * * *
+        // // - - * * *
+        // // - - - *
         // for (int row = 1; row <= n; row++) {
         // for (int col = 1; col <= row - 1; col++) {
-        // System.out.print(" ");
+        // System.out.print("- ");
         // }
         // for (int col = 1; col <= 2 * n - 2 * row + 1; col++) {
         // System.out.print("* ");
@@ -112,8 +110,206 @@ class BasicPattern {
         // System.out.println();
         // }
 
+        // // HOLO RECTANNGLE
+        // // * * * * * *
+        // // * - - - - *
+        // // * - - - - *
+        // // * * * * * *
+        // int n = 4;
+        // for (int row = 1; row <= n; row++) {
+        // for (int col = 1; col <= 6; col++) {
+        // if (row == 1 || row == n) {
+        // System.out.print("* ");
+        // } else {
+        // if (col == 1 || col == 6) {
+        // System.out.print("* ");
+        // } else {
+        // System.out.print("- ");
+        // }
+        // }
+        // }
+        // System.out.println();
+        // }
+
+        // // HOLO TRANGLE
+        // // *
+        // // * *
+        // // * - *
+        // // * - - *
+        // // * * * * *
+        // int n = 5;
+
+        // for (int row = 1; row <= n; row++) {
+        // if (row == 1 || row == 2 || row == n) {
+        // for (int col = 1; col <= row; col++) {
+        // System.out.print("* ");
+        // }
+        // } else {
+        // System.out.print("* ");
+
+        // for (int col = 1; col <= (row - 2); col++) {
+        // System.out.print("- ");
+        // }
+        // System.out.print("* ");
+
+        // }
+        // System.out.println();
+
+        // }
+
+        // // Holo TRANGLE
+        // // - - - - *
+        // // - - - * - *
+        // // - - * - - - *
+        // // - * - - - - - *
+        // // * * * * * * * * *
+        // int n = 5;
+
+        // for (int row = 1; row <= n; row++) {
+        // for (int col = 1; col <= n - row; col++) {
+        // System.out.print("- ");
+        // }
+
+        // if (row==1 || row==n) {
+        // for(int col=1;col<=2*row-1;col++){
+        // System.out.print("* ");
+        // }
+        // }
+        // else{
+        // System.out.print("* ");
+        // for(int col=1;col<=2*row-3;col++){
+        // System.out.print("- ");
+        // }
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+
+        // }
+
+        // // Double Inverted pyramid?
+
+        // // - - - *
+        // // - - * * *
+        // // - * * * * *
+        // // * * * * * * *
+        // // - * * * * *
+        // // - - * * *
+        // // - - - *
+        // int n = 4;
+        // for (int row = 1; row <= n; row++) {
+        // for (int col = 1; col <= n - row; col++) {
+        // System.out.print("- ");
+        // }
+        // for (int col = 1; col <= 2 * row - 1; col++) {
+        // System.out.print("* ");
+
+        // }
+        // System.out.println();
+        // }
+        // for (int row = 1; row <= n; row++) {
+        // if (row==1) {
+        // continue;
+        // }
+        // for (int col = 1; col <= row - 1; col++) {
+        // System.out.print("- ");
+        // }
+        // for (int col = 1; col <= 2 * n - 2 * row + 1; col++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // HOLO DIMOND
+        // // - - - *
+        // // - - * - *
+        // // - * - - - *
+        // // * - - - - - *
+        // // - * - - - *
+        // // - - * - *
+        // // - - - *
+        // int n = 4;
+
+        // for (int row = 1; row <= n; row++) {
+        // for (int col = 1; col <= n - row; col++) {
+        // System.out.print("- ");
+        // }
+        // if (row == 1) {
+        // for (int col = 1; col <= 2 * row - 1; col++) {
+        // System.out.print("* ");
+        // }
+        // } else {
+        // System.out.print("* ");
+        // for (int col = 1; col <= 2 * row - 3; col++) {
+        // System.out.print("- ");
+        // }
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // for(int row =1;row<=(n-1);row++){
+        // for(int col=1;col<=row;col++){
+        // System.out.print("- ");
+        // }
+        // if (row==(n-1)) {
+        // System.out.print("* ");
+        // }
+        // else{
+        // System.out.print("* ");
+        // for(int col=1;col<=2*(n-row)-3;col++){
+        // System.out.print("- ");
+        // }
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // // * - - - - - - *
+        // // * * - - - - * *
+        // // * * * - - * * *
+        // // * * * * * * * *
+        // // * * * * * * * *
+        // // * * * - - * * *
+        // // * * - - - - * *
+        // // * - - - - - - *
+        // int n = 4;
+
+        // for (int row = 1; row <= n; row++) {
+        // // part1
+        // for (int col = 1; col <= row; col++) {
+        // System.out.print("* ");
+        // }
+        // // part2
+        // for (int col = 1; col <= 2 * (n - row); col++) {
+        // System.out.print("- ");
+        // }
+        // // part3
+        // for (int col = 1; col <= row; col++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+
+        // }
+        // for(int row=1;row<=n;row++){
+
+        // //part4
+        // for(int col=1;col<=n-row+1;col++){
+        // System.out.print("* ");
+        // }
+        // // part5
+        // for(int col=1;col<=2*(row-1);col++){
+        // System.out.print("- ");
+        // }
+        // // part6
+        // for(int col=1;col<=n-row+1;col++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+
+
 
         
-
     }
 }
