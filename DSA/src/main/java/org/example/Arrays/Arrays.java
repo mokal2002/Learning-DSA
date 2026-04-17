1f1734282541af218ff905af3c0ffe9bbd09ae66
mokal2002 <aniketmokal29@gmail.com>
2026-04-17 10:43:14 +0000

    2D array basic complete.

--- DSA/src/main/java/org/example/Arrays/Arrays.java
+++ DSA/src/main/java/org/example/Arrays/Arrays.java
@@ -75,13 +75,99 @@ public class Arrays {
         // int minvalue = arrr[0];
 
         // for (int i = 0; i < arrr.length; i++) {
-        //     if (arrr[i] < minvalue) {
-        //         minvalue = arrr[i];
-        //     }
+        // if (arrr[i] < minvalue) {
+        // minvalue = arrr[i];
+        // }
         // }
         // System.out.println(minvalue);
 
-        
+        // 2D Array
+
+        // int[][] arr;
+        // arr = new int[2][3];
+
+        // int[][] brr= {{2,3},{3,4},{4,5}};
+
+        // // System.out.println(brr[0][0]);
+
+        // for(int i=0;i<brr.length;i++){
+        // for(int j =0;j<brr[0].length;j++){
+        // System.out.print(brr[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // int[][] brr= {{2,3},{3,4,3,6},{4,5,3}};
+
+        // for(int i=0;i<brr.length;i++){
+        // for(int j =0;j<brr[i].length;j++){
+        // System.out.print(brr[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // // challange
+        // Scanner sc = new Scanner(System.in);
+        // int[][] userarr = new int[2][3];
+        // for (int i = 0; i < userarr.length; i++) {
+        // for (int j = 0; j < userarr[i].length; j++) {
+        // userarr[i][j] = sc.nextInt();
+        // }
+        // }
+        // for (int i = 0; i < userarr.length; i++) {
+        // for (int j = 0; j < userarr[i].length; j++) {
+        // System.out.print(userarr[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
+        // sc.close();
+
+        // // sum of 2D Arrary
+
+        // int[][] ari= {{2,3,4},{5,6,7},{8,9,0}};
+        // int ans=0;
+        // for (int i = 0; i < ari.length; i++) {
+        // for (int j = 0; j < ari[i].length; j++) {
+        // int value = ari[i][j];
+        // ans = ans +value;
+
+        // }
+        // }
+        // System.out.println(ans);
+
+        // int[][] ari = { { 2, 3, 4 }, { 5, 6, 7 }, { 8, 9, 0 } };
+        // int ans = 1;
+        // for (int i = 0; i < ari.length; i++) {
+        // for (int j = 0; j < ari[i].length; j++) {
+        // int value = ari[i][j];
+        // ans = ans * value;
+
+        // }
+        // }
+        // System.out.println(ans);
+
+        int[][] arr = { { 2, 5, 3 }, { 56, 5, 7 } };
+
+        // int maxvalue = arr[0][0];
+
+        // for (int i = 0; i < arr.length; i++) {
+        // for (int j = 0; j < arr[0].length; j++) {
+        // if (arr[i][j] > maxvalue) {
+        // maxvalue = arr[i][j];
+        // }
+        // }
+        // }
+        // System.out.println(maxvalue);
+
+        // int minvalue = arr[0][0];
+        // for (int i = 0; i < arr.length; i++) {
+        //     for (int j = 0; j < arr[0].length; j++) {
+        //         if (arr[i][j] < minvalue) {
+        //             minvalue = arr[i][j];
+        //         }
+        //     }
+        // }
+        // System.out.println(minvalue);
 
     }
 }
