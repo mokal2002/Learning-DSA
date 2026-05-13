60fd2bb1cca9bf3299d7eacc9d10646a1e3423a1
root <root@localhost.localdomain>
2026-05-13 05:48:14 +0000

    push

--- Problems/src/Arrays/ArrayNextLevl.java
+++ Problems/src/Arrays/ArrayNextLevl.java
@@ -1,7 +1,48 @@
 package Arrays;
 
 public class ArrayNextLevl {
+
+    public static int findMissingNumber(int[] nums){
+        int xorSum = 0;
+        for (int n : nums) {
+            xorSum = xorSum ^ n;
+        }
+        return null;
+    }
+
+    public static int[] get0sand1s(int[] nums){
+        int n = nums.length;
+        int i = 0;
+        int j = n -1;
+
+        while (i < j) {
+            if (nums[i] == 1 && nums[j] == 0) {
+                nums[i] = 0;
+                nums[j] = 1;
+            }
+            if (nums[i] == 0) {
+                i++;
+            }
+            if (nums[j]== 1) {
+                j--;
+            }
+            
+        }
+        return nums;
+    }
     public static void main(String[] args) {
         System.out.println("Array imp questions.");
+
+//Missing Number
+
+        
+
+        // int nums[] = {0,0,1,1,0,1,0,0,1};
+        // int num[] = get0sand1s(nums);
+        // for (int i : num) {
+        //     System.out.print(i + " ");
+        // }
+        // System.out.println();
+        
     }
 }
