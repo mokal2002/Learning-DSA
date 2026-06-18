d485b429486aca67ac0a1fa37685a4b9164b472c
mokal2002 <aniketmokal29@gmail.com>
2026-06-18 13:19:22 +0530

    sum of all cols in 2d array

--- Problems/src/twodarray/SumOfAllColsIn2DArray.java
+++ Problems/src/twodarray/SumOfAllColsIn2DArray.java
@@ -3,31 +3,28 @@ package twodarray;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SumOfAllRowsIn2DArray {
+public class SumOfAllColsIn2DArray {
 
-    public static List<Integer> sumOfRowsIn2DArray(int[][] arr) { // O(m*n)
+    public static List<Integer> sumOfColsIn2DArray(int[][] arr) {
+
         List<Integer> result = new ArrayList<>();
         int m = arr.length;
         int n = arr[0].length;
 
-        // Nested Loop Approch
-        for (int row = 0; row < m; row++) {
-
+        for (int col = 0; col < n; col++) {
             int sum = 0;
-            for (int col = 0; col < n; col++) {
+            for (int row = 0; row < m; row++) {
                 int value = arr[row][col];
                 sum = sum + value;
             }
             result.add(sum);
-
         }
         return result;
     }
 
     public static void main(String[] args) {
+        int arr[][] = { { 10, 2, 3 }, { 2, 3, 4 }, { 3, 4, 5 } };
+        System.out.println(sumOfColsIn2DArray(arr));
 
-        // Sum Of All Rows In 2D Array
-        int arr[][] = { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 5 } };
-        System.out.println(sumOfRowsIn2DArray(arr));
     }
 }
