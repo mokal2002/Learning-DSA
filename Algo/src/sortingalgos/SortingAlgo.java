0cac22ca0feaaf37e56478855f1f9732835c5677
Aniket Mokal <aniketmokal29@gmail.com>
2026-07-03 12:10:57 +0500

    selection sort pronlem

--- Algo/src/sortingalgos/SortingAlgo.java
+++ Algo/src/sortingalgos/SortingAlgo.java
@@ -4,25 +4,55 @@ public class SortingAlgo {
 
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
-        for(int i = 0; i< n-1;i++){
-            for(int j = 0; j < n-i-1;j++){
-                if (arr[j] > arr[j+1]) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1]= temp;
-                    
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
                 }
             }
         }
     }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+
+                }
+                
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+            System.out.println("Round "+ i);
+            for (int value : arr) {
+                    System.out.print(value + " ");
+                }
+                System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[] = {1,3,6,5,4,2};
-        bubbleSort(arr);
+        // int arr[] = { 1, 3, 6, 5, 4, 2 };
+        // bubbleSort(arr);
+        // System.out.println("Printing the Sorted Array.");
+        // for (int value : arr) {
+        // System.out.print(value + " ");
+        // }
+        // System.out.println();
+
+        int arr[] = { 3, 1, 5, 4, 2 };
+        selectionSort(arr);
         System.out.println("Printing the Sorted Array.");
         for (int value : arr) {
-            System.out.println(value + " ");
+            System.out.print(value + " ");
         }
-    
-
+        System.out.println();
     }
 }
