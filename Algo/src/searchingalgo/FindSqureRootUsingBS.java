de9e4fd0c5dbacfeb815139ceaa044fb6711736d
Aniket Mokal <aniketmokal29@gmail.com>
2026-08-07 14:06:48 +0500

    Preision no

--- Algo/src/searchingalgo/FindSqureRootUsingBS.java
+++ Algo/src/searchingalgo/FindSqureRootUsingBS.java
@@ -4,27 +4,45 @@ public class FindSqureRootUsingBS {
     public static int findSqurRoot(int x) {
         int s = 1;
         int e = x;
-        int ans = -1;
+        double ans = -1;
 
-        if(x == 0){
+        if (x == 0) {
             return 0;
         }
 
         while (s <= e) {
             int mid = s + (e - s) / 2;
-            if (mid == x/mid) {
+            if (mid == x / mid) {
                 return mid;
-            } else if (mid > x/mid) {
+            } else if (mid > x / mid) {
                 e = mid - 1;
             } else {
                 ans = mid;
                 s = mid + 1;
             }
         }
+
+        double factor = 1;
+        int totalPrecsion = 3;
+        for (int round = 1; round <= totalPrecsion; round++) {
+            factor = factor / 10;
+            for (int i = 1; i <= 10; i++) {
+                double newAns = ans + factor;
+                // if (newAns * newAns == x) {
+                //     return -1;
+                // } 
+                if (newAns * newAns < x) {
+                    ans = newAns;
+                } else {
+                    break;
+                }
+            }
+            System.out.println("For The First Round Ans: "+ ans);
+        }
         return ans;
     }
 
     public static void main(String[] args) {
-        System.out.println(findSqurRoot(214739560));
+        System.out.println(findSqurRoot(56));
     }
 }
